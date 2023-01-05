934fe2b9f20ab950d9bf8ef0328e4014d7f71aa6
Mehrdadfaridi91 <119711667+Mehrdadfaridi91@users.noreply.github.com>
2023-01-05 14:33:24 +0330

    first

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CW6_2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            void PrintWelcome()
+            {
+                Console.WriteLine("Welcome!");
+            }
+            PrintWelcome();
+        }
+    }
+}
